c2c72febb38a4b350dacd26e5c6f42ef3a4999e7
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-06-18 15:55:15 +0530

    sdsD

--- vendor/filters.py
+++ vendor/filters.py
@@ -8,7 +8,7 @@ class TripFilter(django_filters.FilterSet):
     pickup_city = django_filters.CharFilter(field_name='pickup_city', lookup_expr='icontains')
     mode_of_transport = django_filters.CharFilter(field_name='mode_of_transport', lookup_expr='exact')
     parcel_capacity = django_filters.NumberFilter(field_name='parcel_capacity', lookup_expr='gte')
-    departure_datetime = django_filters.DateFilter(field_name='travelling_date', lookup_expr='exact')
+    departure_datetime = django_filters.DateFilter(field_name='departure_datetime', lookup_expr='exact')
     arrival_datetime = django_filters.DateFilter(field_name='arrival_datetime', lookup_expr='exact')
 
     class Meta:
