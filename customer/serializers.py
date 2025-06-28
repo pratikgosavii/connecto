b182b2e0cd0ba0fd9042f2c96686958dad40a12f
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-06-28 22:02:38 +0530

    sdsD

--- customer/serializers.py
+++ customer/serializers.py
@@ -59,5 +59,5 @@ class RequestVendorForDeliverySerializer(serializers.ModelSerializer):
 class DeliveryRatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = DeliveryRating
-        fields = ['id', 'agent', 'user', 'rating', 'feedback', 'created_at']
+        fields = '__all__"'
         read_only_fields = ['id', 'user', 'created_at']
