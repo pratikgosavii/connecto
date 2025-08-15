b09915df24f088e96dab9a0992cff23e2302593b
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-08-15 18:50:49 +0530

    Stop tracking Report.csv and Report.xlsx

--- customer/views.py
+++ customer/views.py
@@ -773,7 +773,7 @@ from django.db import transaction
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from .models import PaymentLog, UserCredit
+from .models import PaymentLog
 
 # -----------------------------
 # Credit packages
