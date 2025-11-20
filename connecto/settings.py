fd7fe4de87d816943ccffbcd1b02ce4b50ad6ac4
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-11-20 17:14:33 +0530

    dfddf

--- connecto/settings.py
+++ connecto/settings.py
@@ -107,8 +107,8 @@ cred = credentials.Certificate(firebase_key_path)
 initialize_app(cred)
 
 
-RAZORPAY_KEY_ID = 'rzp_test_R5XV4nQlMkdVy8'
-RAZORPAY_KEY_SECRET = 'Hf5dz8SL9EW72SfHwpq1e540'
+RAZORPAY_KEY_ID = 'rzp_test_Rhy8NBxUoi3UsE'
+RAZORPAY_KEY_SECRET = 'I44vrD209dFoS24gtwdi9jVx'
 RAZORPAY_WEBHOOK_SECRET='s8f7s9df87s9df87s9df87s9df87s9df8'
 
 STREAM_API_KEY = "c7wwttj85hg7"
