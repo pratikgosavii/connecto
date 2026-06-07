dfdce846003f9f12c98242f2bfead01e54929153
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2026-06-07 16:57:47 +0530

    sdsd

--- connecto/settings.py
+++ connecto/settings.py
@@ -257,8 +257,8 @@ STATIC_URL = '/static/'
 
 
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_USER_MODEL = 'users.User'
 
